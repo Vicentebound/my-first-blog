2df48140ad409ffe6210f31955cba9eff89e7ef7
Vicente <vicentedefigueiredo@gmail.com>
2016-02-29 17:28:04 -0300

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils import timezone
-
+"""
+class Post(models.Model): - esta linha define o nosso modelo (é um objeto).
+class é uma palavra-chave especial que indica que estamos definindo um objeto.
+Post é o nome do nosso modelo, podemos lhe dar um nome diferente (mas é preciso evitar os espaços em branco e caracteres especiais). Sempre comece um nome de classe com uma letra maiúscula.
+models.Model significa que o Post é um modelo de Django, então o Django sabe ele que deve ser salvo no banco de dados.
+"""
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
